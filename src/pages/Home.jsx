7161bae35bc6dce8615224a29aa6766c7844fe04
Camila Germano Rodrigues <camyrodrigues23@gmail.com>
2026-02-11 14:57:43 -0300

    adicionando navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import { ThemeToggle } from "../components/ThemeToggle"
 import { StarBackgound } from "../components/StarBackground"
+import { Navbar } from "../components/Navbar"
 
 export const Home = () => {
     return( 
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             <ThemeToggle/>
             <StarBackgound/>
-
+            <Navbar/>
 
         </div>
     )
